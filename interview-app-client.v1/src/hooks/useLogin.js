74ad97d3f4b3eacc0771051dae92f38a606ef396
dzondra <djole.nic@gmail.com>
2022-01-06 22:54:38 +0100

    Login set token

--- interview-app-client.v1/src/hooks/useLogin.js
+++ interview-app-client.v1/src/hooks/useLogin.js
@@ -1,23 +1,25 @@
 import { useState } from "react";
 import validator from "validator";
 import { Alert } from "react-bootstrap";
-import Api from '../services/Service'
+import Api from "../services/Service";
 
-export default function useToken() {
-
-
+export default function useLogin() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState("");
-  const api = new Api()
+  const api = new Api();
 
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
     const errorsList = validateFormData();
     const errorsToDisplay = buildErrorList(errorsList);
-    setErrors((e) => (e = errorsToDisplay)) 
-    errorsList.length == 0 && api.get("asd")
+    setErrors((e) => (e = errorsToDisplay));
+    if (errorsList.length > 0) return;
+    const response = await api.post("login", { email, password })
+    console.log(response);
+    const { error, token } = response
+    if(error) setErrors(buildErrorList([error])) 
+    if(token) alert("success")
   };
-
 
   const buildErrorList = (errorsList) => {
     const errs = errorsList.map((value, key) => {
@@ -31,15 +33,13 @@ export default function useToken() {
     ) : null;
   };
 
-
   // returns errors array
   const validateFormData = () => {
     var errors = [];
     !validator.isEmail(email) && errors.push("Not a valid email");
-    !validator.isStrongPassword(password) && errors.push("Please enter a strong password");
+    // !validator.isStrongPassword(password) && errors.push("Please enter a strong password");
     return errors;
   };
-
 
   return {
     handleSubmit,
