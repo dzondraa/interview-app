935b6a37dfc2bd19acf7fbae1a4b6e365c27932f
dzondra <djole.nic@gmail.com>
2022-01-28 09:21:22 +0100

    Error handle

--- interview-app-client.v1/src/components/Dashboard/SearchDocForm.js
+++ interview-app-client.v1/src/components/Dashboard/SearchDocForm.js
@@ -13,11 +13,8 @@ const SearchDocForm = ({
 
   const submitButton = () => {
     const errorList = validate();
-    if (errorList.length) {
-      setErrors((e) => (e = errorList));
-      return;
-    }
-
+    setErrors((e) => (e = errorList));
+    if (errorList.length) return;
     api
       .get(
         `documents?path=${config.FILE_REPOSITORY}&targetMatch=${wordCount}&keyword=${keyword}`
@@ -46,44 +43,46 @@ const SearchDocForm = ({
   };
 
   return (
-    <form className="form-inline">
-      <div className="form-group mb-2">
-        <label htmlFor="staticEmail2" className="sr-only">
-          Email
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          id="staticEmail2"
-          placeholder="Keyword"
-          onChange={(e) => {
-            setKeyword((kw) => (kw = e.target.value));
-          }}
-        />
-      </div>
-      <div className="form-group mx-sm-3 mb-2">
-        <label htmlFor="inputPassword2" className="sr-only">
-          Password
-        </label>
-        <input
-          type="number"
-          className="form-control"
-          id="inputPassword2"
-          placeholder="Word count"
-          onChange={(e) => {
-            setWordCount((wc) => (wc = e.target.value));
-          }}
-        />
-      </div>
-      <button
-        onClick={submitButton}
-        type="button"
-        className="btn btn-primary mb-2"
-      >
-        Scrape!
-      </button>
+    <>
+      <form className="form-inline">
+        <div className="form-group mb-2">
+          <label htmlFor="staticEmail2" className="sr-only">
+            Email
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="staticEmail2"
+            placeholder="Keyword"
+            onChange={(e) => {
+              setKeyword((kw) => (kw = e.target.value));
+            }}
+          />
+        </div>
+        <div className="form-group mx-sm-3 mb-2">
+          <label htmlFor="inputPassword2" className="sr-only">
+            Password
+          </label>
+          <input
+            type="number"
+            className="form-control"
+            id="inputPassword2"
+            placeholder="Word count"
+            onChange={(e) => {
+              setWordCount((wc) => (wc = e.target.value));
+            }}
+          />
+        </div>
+        <button
+          onClick={submitButton}
+          type="button"
+          className="btn btn-primary mb-2"
+        >
+          Scrape!
+        </button>
+      </form>
       {errors.length ? <ErrorBox errors={errors} /> : null}
-    </form>
+    </>
   );
 };
 
--- interview-app-client.v1/src/components/common/ErrorBox/ErrorBox.js
+++ interview-app-client.v1/src/components/common/ErrorBox/ErrorBox.js
@@ -1,6 +1,6 @@
 const ErrorBox = ({ errors }) => {
   return (
-    <div class="alert alert-danger" role="alert">
+    <div className="alert alert-danger" role="alert">
       {errors.map((error, index) => {
         return <li key={index}>{error.message}</li>;
       })}
