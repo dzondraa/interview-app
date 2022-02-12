79417b272233a785d898c44003756896b8baf251
dzondra <djole.nic@gmail.com>
2022-02-13 00:47:44 +0100

    Add tests to project | JEST

---
+++ interview-app-client/src/test/Link.react.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {cleanup, fireEvent, render} from '@testing-library/react';
+import Areas from '../pages/Areas/Areas';
+
+// Note: running cleanup afterEach is done automatically for you in @testing-library/react@9.0.0 or higher
+// unmount and cleanup DOM after the test is finished.
+afterEach(cleanup);
+
+// TODO -> Finihs unit test
+it('Renders successfully', () => {
+
+});
