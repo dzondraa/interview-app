b35f97a11829d0756503a532677dbd76bf38eb4b
dzondra <djole.nic@gmail.com>
2022-02-13 21:22:02 +0100

    AMQ messaging lib

--- live-interview-service/index.js
+++ live-interview-service/index.js
@@ -3,28 +3,10 @@ const http = require("http");
 const router = require("./router");
 var cors = require("cors");
 const PORT = process.env.PORT || 5000;
-const amqp = require("amqplib/callback_api");
+require("./messagingService");
 
 // Connect to message broker
-amqp.connect("amqp://localhost", function (error0, connection) {
-  if (error0) {
-    throw error0;
-  }
-  connection.createChannel(function (error1, channel) {
-    if (error1) {
-      throw error1;
-    }
-    var queue = "interview";
-    var msg = { interviewData: { q1: "asdasd" } };
-
-    // channel.assertQueue(queue, {
-    //   durable: false
-    // });
-
-    channel.sendToQueue(queue, Buffer.from(JSON.stringify(msg)));
-    console.log(" [x] Sent %s", msg);
-  });
-});
+var answersGlobal = [];
 
 var app = express();
 app.use(cors());
@@ -48,14 +30,19 @@ io.on("connection", (socket) => {
   socket.on("endInterview", ({ interviewId }, callback) => {
     socket.broadcast.to(interviewId).emit("interviewEnded", null);
     socket.join(interviewId);
-
+    notifyResourceService(
+      JSON.stringify({
+        interviewId: interviewId,
+        answers: answersGlobal,
+      })
+    );
     callback();
   });
 
   socket.on("answerUpdate", ({ answers, interviewId }, callback) => {
     socket.broadcast.to(interviewId).emit("recievedAnwer", answers);
     socket.join(interviewId);
-
+    answersGlobal = answers;
     callback({ type: "Info", message: "Answer saved!" });
   });
 
---
+++ live-interview-service/messagingService.js
@@ -0,0 +1,22 @@
+const amqp = require("amqplib/callback_api");
+
+module.exports = notifyResourceService = (message) => {
+  amqp.connect("amqp://localhost", function (error0, connection) {
+    if (error0) {
+      throw error0;
+    }
+    connection.createChannel(function (error1, channel) {
+      if (error1) {
+        throw error1;
+      }
+      var queue = "interview";
+
+      // channel.assertQueue(queue, {
+      //   durable: false
+      // });
+
+      channel.sendToQueue(queue, Buffer.from(message));
+      console.log(" [x] Sent %s", message);
+    });
+  });
+};
