8b0cb88216939f610830b50cae096ff13040f5b1
dzondra <djole.nic@gmail.com>
2022-02-13 17:40:18 +0100

    Add form logic

--- interview-app-client/src/components/Dashboard/Modals/NewInterviewModal.js
+++ interview-app-client/src/components/Dashboard/Modals/NewInterviewModal.js
@@ -7,13 +7,46 @@ import NewInterviewForm from "../../Interview/NewInterviewForm";
 
 const NewInterviewModal = ({ modalOpen, handleClose }) => {
   const api = ApiFactory.getResourceApiInstance();
-  const [interviewDetails, setInterviewDetails] = useState(null);
   const [areas, setAreas] = useState([]);
+  const [questions, setQuestions] = useState([]);
+  const [selectedAreas, setSelectedAreas] = useState([]);
+
+  const updateAreas = (e) => {
+    var selectedArrState = selectedAreas;
+    e.target.checked
+      ? selectedArrState.push(e.target.value)
+      : selectedArrState.indexOf(e.target.value) > -1 &&
+        selectedArrState.splice(selectedArrState.indexOf(e.target.value), 1);
+    setSelectedAreas((s) => (s = selectedArrState));
+  };
+  const checkChange = async (e) => {
+    updateAreas(e);
+    try {
+      const questionsResponse = await api.get("question" + buildUrlParams());
+      setQuestions(questionsResponse);
+    } catch (ex) {
+      console.error(ex);
+    }
+  };
+
+  const buildUrlParams = () => {
+    if (selectedAreas.length === 0) return "";
+    var str = "?";
+    selectedAreas.forEach((area) => {
+      str += `area=${area}&`;
+    });
+    return str;
+  };
   useEffect(() => {
-    api
-      .get("area")
-      .then((areas) => setAreas(areas))
-      .catch((er) => console.log(er.message));
+    const getAreas = async () => {
+      try {
+        const areasResponse = await api.get("area");
+        setAreas(areasResponse);
+      } catch (ex) {
+        console.error(ex);
+      }
+    };
+    getAreas();
   }, []);
 
   useEffect(() => {}, []);
@@ -28,9 +61,9 @@ const NewInterviewModal = ({ modalOpen, handleClose }) => {
         <Modal.Title>Interview details</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <Area area={areas}></Area>
-        <div className="form-container col-lg-6">
-          <NewInterviewForm />
+        <Area checkChange={checkChange} area={areas}></Area>
+        <div className="form-container col-lg-7">
+          <NewInterviewForm questions={questions} />
         </div>
       </Modal.Body>
       <Modal.Footer>
--- interview-app-client/src/components/Interview/NewInterviewForm.js
+++ interview-app-client/src/components/Interview/NewInterviewForm.js
@@ -1,42 +1,48 @@
-const NewInterviewForm = () => {
+const NewInterviewForm = ({ questions }) => {
   return (
     <form>
-      <div class="form-group">
-        <label for="exampleFormControlInput1">Email address</label>
+      <div className="form-group">
+        <label htmlFor="exampleFormControlInput1">Candidate</label>
         <input
-          type="email"
-          class="form-control"
+          type="text"
+          className="form-control"
           id="exampleFormControlInput1"
-          placeholder="name@example.com"
+          placeholder="Candidate email"
         />
       </div>
-      <div class="form-group">
-        <label for="exampleFormControlSelect1">Example select</label>
-        <select class="form-control" id="exampleFormControlSelect1">
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
+      <div className="form-group">
+        <label htmlFor="exampleFormControlSelect2">
+          Select questions for the
+        </label>
+        <select
+          multiple
+          className="form-control"
+          id="exampleFormControlSelect2"
+        >
+          {questions.map((question, key) => (
+            <option title={question.description} key={key} value={question.id}>
+              {question.name}
+            </option>
+          ))}
         </select>
       </div>
-      <div class="form-group">
-        <label for="exampleFormControlSelect2">Example multiple select</label>
-        <select multiple class="form-control" id="exampleFormControlSelect2">
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
-        </select>
+      <div className="form-group">
+        <label htmlFor="date">Date</label>
+        <input
+          type="date"
+          className="form-control"
+          id="date"
+          placeholder="Date"
+        />
       </div>
-      <div class="form-group">
-        <label for="exampleFormControlTextarea1">Example textarea</label>
-        <textarea
-          class="form-control"
-          id="exampleFormControlTextarea1"
-          rows="3"
-        ></textarea>
+      <div className="form-group">
+        <label htmlFor="time">Example textarea</label>
+        <input
+          type="time"
+          className="form-control"
+          id="time"
+          placeholder="Time"
+        />
       </div>
     </form>
   );
